79dd68b49758e487a0c2a57765fafa0d97a4cc4b
idan <idanb11@gmail.com>
2014-10-21 21:04:00 +0700

    Update infinite-scroll.js
    Hi,
    
    I added timeout attribute which specifies how much time (in ms) to wait between each call of the infiniteScroll function.
    Need to update to readme.md too.

--- src/infinite-scroll.js
+++ src/infinite-scroll.js
@@ -1,15 +1,26 @@
 angular.module('infiniteScroll', [])
-    .directive('infiniteScroll', [ "$window", function ($window) {
-        return {
-            link:function (scope, element, attrs) {
-                var offset = parseInt(attrs.threshold) || 0;
-                var e = element[0];
+    .directive('infiniteScroll', [ "$timeout", function ($timeout) {
+    "use strict";
 
-                element.bind('scroll', function () {
-                    if (scope.$eval(attrs.canLoad) && e.scrollTop + e.offsetHeight >= e.scrollHeight - offset) {
-                        scope.$apply(attrs.infiniteScroll);
-                    }
-                });
+    return {
+      link:function (scope, element, attrs) {
+        var offset = parseInt(attrs.threshold) || 0;
+        var e = element[0];
+        var timeout = attrs.timeout || 500;
+        var loadTimer = false;
+
+        element.bind('scroll', function () {
+          if (scope.$eval(attrs.canLoad) && e.scrollTop + e.offsetHeight >= e.scrollHeight - offset) {
+
+            if (loadTimer) {
+              $timeout.cancel(loadTimer);
             }
-        };
-    }]);
+
+            loadTimer = $timeout(function () {
+              scope.$apply(attrs.infiniteScroll);
+            }, timeout);
+          }
+        });
+      }
+    };
+  }]);
